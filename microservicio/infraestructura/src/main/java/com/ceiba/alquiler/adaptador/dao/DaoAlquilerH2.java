0c0c0114baa664335b8aa659311a9687700287d6
Jeisson Rangel <jeisson.rangel@ceiba.com.co>
2022-02-17 14:41:57 -0500

    DAO Alquiler para H2 metodos listar y buscar por id implementados

--- microservicio/infraestructura/src/main/java/com/ceiba/alquiler/adaptador/dao/DaoAlquilerH2.java
+++ microservicio/infraestructura/src/main/java/com/ceiba/alquiler/adaptador/dao/DaoAlquilerH2.java
@@ -6,7 +6,9 @@ import com.ceiba.infraestructura.jdbc.CustomNamedParameterJdbcTemplate;
 import com.ceiba.infraestructura.jdbc.sqlstatement.SqlStatement;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Component
 public class DaoAlquilerH2 implements DaoAlquiler {
@@ -14,7 +16,7 @@ public class DaoAlquilerH2 implements DaoAlquiler {
     private final CustomNamedParameterJdbcTemplate customNamedParameterJdbcTemplate;
 
     @SqlStatement(namespace="alquiler", value="listar")
-    private static String sqlLista;
+    private static String sqlListar;
 
     @SqlStatement(namespace = "alquiler", value="buscaId")
     private static String sqlBuscarId;
@@ -25,11 +27,14 @@ public class DaoAlquilerH2 implements DaoAlquiler {
 
     @Override
     public List<DtoAlquiler> listar() {
-        return this.customNamedParameterJdbcTemplate.getNamedParameterJdbcTemplate().queryForList();
+        return this.customNamedParameterJdbcTemplate.getNamedParameterJdbcTemplate().query(sqlListar, new MapeoAlquiler());
     }
 
     @Override
     public DtoAlquiler buscarPorId(Long id) {
-        return null;
+        Map<String, Object> parametros = new HashMap<>();
+        parametros.put("id",id);
+
+        return this.customNamedParameterJdbcTemplate.getNamedParameterJdbcTemplate().queryForObject(sqlBuscarId,parametros, new MapeoAlquiler());
     }
 }
