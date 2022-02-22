7afb78f3501d70443bbea07d7e985fd4c3cfc827
Jeisson Rangel <jeisson.rangel@ceiba.com.co>
2022-02-22 09:26:47 -0500

    Error en return metodo listar consultacontrolador factura

--- microservicio/infraestructura/src/main/java/com/ceiba/alquiler/controlador/ConsultaControladorFactura.java
+++ microservicio/infraestructura/src/main/java/com/ceiba/alquiler/controlador/ConsultaControladorFactura.java
@@ -1,7 +1,7 @@
 package com.ceiba.alquiler.controlador;
 
 import com.ceiba.alquiler.consulta.ManejadorListarFacturas;
-import com.ceiba.alquiler.modelo.dto.DtoAlquiler;
+import com.ceiba.alquiler.modelo.dto.DtoFactura;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +18,7 @@ public class ConsultaControladorFactura {
     }
 
     @GetMapping
-    public List<DtoAlquiler> listar(){
+    public List<DtoFactura> listar(){
         return this.manejadorListarFacturas.ejecutar();
     }
 }
